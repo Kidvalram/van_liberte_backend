ce71e39df042c92f845dd4517a52a56a43042974
Amaury Valdivia <valram@domory.co>
2021-06-27 17:36:55 +0200

    van backend v1.0.2

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,6 +5,8 @@ module.exports = function(app) {
   app.use(function(req, res, next) {
     res.header(
       "Access-Control-Allow-Headers",
+      "Access-Control-Allow-Methods",
+      "Access-Control-Allow-Origin",
       "x-access-token, Origin, Content-Type, Accept"
     );
     next();
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(Cors());
 // parse requests of content-type - application/json
-app.use(express.json());
+app.use(express.json()); 
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
