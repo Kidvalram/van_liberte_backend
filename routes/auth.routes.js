edf36bc1e7af288b9beb3184a7c4041978252f6a
Amaury Valdivia <valram@domory.co>
2021-06-28 09:51:52 +0200

    van backend v1.0.5

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -5,10 +5,9 @@ module.exports = function(app) {
   app.use(function(req, res, next) {
     res.header(
       "Access-Control-Allow-Headers",
-      "Access-Control-Allow-Methods",
-      "Access-Control-Allow-Origin",
       "x-access-token, Origin, Content-Type, Accept"
     );
+    res.header("Access-Control-Allow-Origin", "*");
      next();
   });
 
